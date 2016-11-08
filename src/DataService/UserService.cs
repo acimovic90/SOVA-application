0cf2eff234ab3185cfd536f65768a5c645a30875
Kristoffer Winther Jessen <krelle@cnfa.dk>
2016-11-08 18:22:09 +0100

    UserService tilføjet

---
+++ src/DataService/UserService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DomainModels.Models;
+
+namespace DataService
+{
+    public class UserService : IUserService
+    {
+        public User GetUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
