17c4d40eb4b6473c10cbc04e84ea2dafb71449f8
Kristoffer Winther Jessen <krelle@cnfa.dk>
2016-11-08 18:16:54 +0100

    IUserService interface tilføjet

---
+++ src/DataService/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DomainModels.Models;
+
+namespace DataService
+{
+    interface IUserService
+    {
+        IList<User> GetUsers();
+        User GetUserById(int id);
+    }
+}
