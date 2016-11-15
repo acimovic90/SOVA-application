327e0d4f4d066660d4476200922862744f6bcb4a
Kristoffer Winther Jessen <krelle@cnfa.dk>
2016-11-15 16:21:20 +0100

    Klargøring til tests i ProjectPortfolio2 solution

---
+++ src/ProjectPortfolio2/Tests/TestExample.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProjectPortfolio2
+{
+    public class TestExample
+    {
+        [Fact]
+        public void Type_WhatToDo_ExpectedResult()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
