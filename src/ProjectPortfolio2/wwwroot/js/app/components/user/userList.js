5f026c1f5acd72ce672b4740d74f9f6a4982e074
acimovic90 <acimovic90@gmail.com>
2016-12-12 16:29:14 +0100

    jquery

--- src/ProjectPortfolio2/wwwroot/js/app/components/user/userList.js
+++ src/ProjectPortfolio2/wwwroot/js/app/components/user/userList.js
@@ -1,8 +1,12 @@
 ﻿define(['knockout', 'dataservice', 'jquery','postman', 'config'], function (ko, dataService,$,postman, config) {
     return function () {
         $("#new__user").hide();
-        $(".new__user__button").click(function () {
+        $(".new__user__button").click(function (e) {
             $("#new__user").show();
+        });
+
+        $(".button__success").click(function (e) {
+            $("#new__user").hide();
         });
         var users = ko.observableArray([]);
 
