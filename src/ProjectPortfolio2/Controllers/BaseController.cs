f9f07633f0556ca751355f58c2f8a629ecbd310f
acimovic90 <acimovic90@gmail.com>
2017-01-21 14:01:47 +0100

    check

--- src/ProjectPortfolio2/Controllers/BaseController.cs
+++ src/ProjectPortfolio2/Controllers/BaseController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProjectPortfolio2.Controllers
 {
-    public class BaseController : Controller
+    public class BaseController : Controller //
     {
         protected bool IsLastPage(int page, int pageSize, int total)
         {
